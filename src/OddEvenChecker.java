96b757a36fdf8d27f12baec5e3669a59a6ffce8c
KwakuSekyere <sekyerko@mail.uc.edu>
2024-02-04 20:52:17 -0500

    Initial commit

---
+++ src/OddEvenChecker.java
@@ -0,0 +1,11 @@
+public class OddEvenChecker {
+    public static void main(String[] args) {
+        int numToExamine = 2;
+
+        if (numToExamine % 2 == 0) {
+            System.out.println(numToExamine + " is Even.");
+        } else {
+            System.out.println(numToExamine + " is Odd.");
+        }
+    }
+}
